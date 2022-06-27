d1a62ea500f5be6cbb0e0ec9bab05fd1209e7f9a
Ruslan Ignatov <ruslanlena30@gmail.com>
2022-06-27 12:03:22 +0300

    Secret key to env

--- dnd_db_site/settings.py
+++ dnd_db_site/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e8_mgg=!)2-#0v*nf-fwity(fhvqfu_ek1vvc1j6%q9g8%z51#'
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
